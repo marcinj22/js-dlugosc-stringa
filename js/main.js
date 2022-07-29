953e57add0d99f4ed884f8539aa97d582fc26c5e
marcinj <marcin.jurewicz@gmail.com>
2022-07-29 14:39:17 +0200

    poprawa zadania

--- js/main.js
+++ js/main.js
@@ -6,9 +6,9 @@ let name2 = "Jestem świetnym programistą";
 function longerThan(lenght1, lenght2) {
 
     if (name1.length > name2.length) {
-        return "name1";
+        return `${name1}`;
     } else if (name2.length > name1.length) {
-        return "name2";
+        return `${name2}`;
     } else {
         "są równe";
     }
